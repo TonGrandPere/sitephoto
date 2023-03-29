100cda917311a0d904072bb4033ed4bd03a5f48d
TonGrandPere <113916041+TonGrandPere@users.noreply.github.com>
2023-03-29 08:30:08 +0200

    fix2

--- Pour le site PHP/display_products.php
+++ Pour le site PHP/display_products.php
@@ -6,11 +6,11 @@ use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 
 // Specify your connection string and create a blob client instance
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=<your_account_name>;AccountKey=<your_account_key>";
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=blobeuh;AccountKey=W0xlWcNRbHNGq+FbkXYgHFehIjqeUqFNSSLReiLA26oXxHKixzmz/gaYH7nciLD/D1g1qliCormm+ASt1cniOQ==";
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
 // Specify the container name that you want to list blobs from
-$containerName = "<your_container_name>";
+$containerName = "images";
 
 try {
     // Get list of all blobs in the container
